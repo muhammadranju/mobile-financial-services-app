a2aebc7af055a16f55f288d0ce78fe398355417b
Muhammad Ranju <mdranju23@gmail.com>
2024-09-21 22:12:25 +0600

    Fix: correct configuration for view engine and static files

--- app/app.js
+++ app/app.js
@@ -2,20 +2,13 @@ const express = require("express");
 const ejs = require("ejs");
 const middleware = require("../middleware/middleware");
 const router = require("../routes");
-const path = require("path");
 const app = express();
 
 // Correct configuration for the view engine
-// app.engine("ejs", ejs.renderFile);
-// app.set("view engine", "ejs"); // Corrected: Pass 'ejs' as a string
+app.engine("ejs", ejs.renderFile);
+app.set("view engine", "ejs"); // Corrected: Pass 'ejs' as a string
 
-// app.use(express.static("public"));
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.use([middleware, router]);
 
